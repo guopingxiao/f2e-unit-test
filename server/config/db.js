e9a0ea75043e230bf03f9bf182b6be59a4ee184d
xiaoguoping <xiaoguoping@meituan.com>
2018-07-23 00:23:50 +0800

    fixed

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,8 @@
 import '../../env'
 import Sequelize from 'sequelize'
 
+// 使用url连接的形式进行连接，注意将root: 后面的XXXX改成自己数据库的密码
+// const Todolist = new Sequelize('mysql://root:XXXX@localhost/todolist'
 const Todolist = new Sequelize(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL || 'localhost'}/todolist`, {
   define: {
     timestamps: false // 取消Sequelzie自动给数据表加入时间戳（createdAt以及updatedAt）
